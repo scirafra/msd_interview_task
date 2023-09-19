610a953ff5257f74842a7bd847f0bcca545d4228
scirafra <83989480+scirafra@users.noreply.github.com>
2023-09-19 11:48:50 +0200

    Update task.py

--- task.py
+++ task.py
@@ -1,21 +1,22 @@
 from collections import Counter
+from string import *
 
 filename = "text.txt"
-top_results=10
-
-with open(filename, "r", encoding="utf8") as f:
-    inp = f.read()
-    f.close()
-    
-inp = inp.lower()
-inp = inp.split()
+top_results = 10
 double_words = []
 
-for i in range(len(inp) - 1):
-    double_words.append(inp[i] + " " + inp[i + 1])
+with open(filename, "r", encoding="utf8") as f:
     
-Counter = Counter(double_words)
-most_occur = Counter.most_common(top_results)
+    inp = f.read()
+    inp = inp.translate(str.maketrans("", "", punctuation))
+    inp = inp.lower().split()
+    if len(inp) > 1:
 
-for i in range(top_results):
-    print(most_occur[i])
+        for i in range(len(inp) - 1):
+            double_words.append(inp[i] + " " + inp[i + 1])
+
+        Counter = Counter(double_words)
+        most_occur = Counter.most_common(top_results)
+
+        for i in range(min(top_results, len(most_occur))):
+            print(most_occur[i])
